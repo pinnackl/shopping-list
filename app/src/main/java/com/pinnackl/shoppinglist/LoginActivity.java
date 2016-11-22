ad061fa61121fd8854d0928df833cdc79b17d999
alexis thorel <alexis.thorel@gmail.com>
2016-11-23 00:15:31 +0100

    [feature] add register activity and link to register view from login page

--- app/src/main/java/com/pinnackl/shoppinglist/LoginActivity.java
+++ app/src/main/java/com/pinnackl/shoppinglist/LoginActivity.java
@@ -3,6 +3,7 @@ package com.pinnackl.shoppinglist;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -92,6 +93,15 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+        //Open register activity
+        TextView register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
     }
 
     private void populateAutoComplete() {
