57aea6b96ed943f968aefab93f4a95f89a837f52
alexis thorel <alexis.thorel@gmail.com>
2016-11-23 01:06:14 +0100

    get values form register

--- app/src/main/java/com/pinnackl/shoppinglist/RegisterActivity.java
+++ app/src/main/java/com/pinnackl/shoppinglist/RegisterActivity.java
@@ -1,13 +1,51 @@
 package com.pinnackl.shoppinglist;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+    private EditText mFirstnameView;
+    private EditText mLastnameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mFirstnameView = (EditText) findViewById(R.id.firstname);
+        mLastnameView = (EditText) findViewById(R.id.lastname);
+
+        Button mRegisterButton = (Button) findViewById(R.id.email_register_button);
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptRegister();
+            }
+        });
+    }
+
+    private void attemptRegister() {
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+        String firstname = mFirstnameView.getText().toString();
+        String lastname = mLastnameView.getText().toString();
+
+        Context context = getApplicationContext();
+        CharSequence text = email;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
