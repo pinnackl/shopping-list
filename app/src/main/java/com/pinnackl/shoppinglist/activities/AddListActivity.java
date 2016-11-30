28b3511aec52f25f632a2eac3ee98e705738bdbc
athorel <alexis.thorel@gmail.com>
2016-11-30 17:12:18 +0100

    wip create shopping list

--- app/src/main/java/com/pinnackl/shoppinglist/activities/AddListActivity.java
+++ app/src/main/java/com/pinnackl/shoppinglist/activities/AddListActivity.java
@@ -2,14 +2,49 @@ package com.pinnackl.shoppinglist.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.pinnackl.shoppinglist.R;
 
 public class AddListActivity extends AppCompatActivity {
+    private EditText mListNameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mListNameView = (EditText) findViewById(R.id.listName);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_list);
+
+        Button createListButton = (Button) findViewById(R.id.button);
+        createListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createList();
+            }
+        });
+    }
+
+    public boolean createList() {
+        String listName = mListNameView.getText().toString();
+        View focusView = null;
+        boolean cancel = false;
+
+        if (TextUtils.isEmpty(listName)) {
+            mListNameView.setError(getString(R.string.error_field_required));
+            focusView = mListNameView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            focusView.requestFocus();
+        } else {
+
+        }
+
+        return true;
     }
 }
